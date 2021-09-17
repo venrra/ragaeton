188050f66c92f3aa754492f64ba7c731b0c97001
Manu Hidalgo Carmona <manuelhicar@correo.ugr.es>
2021-09-17 11:22:34 +0200

    modificado nuevo archivo de prueba

--- pkg/direccion/direccion.go
+++ pkg/direccion/direccion.go
@@ -1,5 +1,16 @@
 package direccion
 
-type Direccion struct{
+import (
+	"regexp"
+)
 
+type Direccion struct {
+	ip      string
+	address string
+	Ipad
+}
+
+func (dir Direccion) comprobar_ip_valida() (bool, error) {
+	expresion_ip := "^[0-255].[0-255].[0-255].[0-255]$"
+	return regexp.MatchString(expresion_ip, dir.ip)
 }
